1a629cca8aa3b15e368d3d9cccd70de15a4a9204
cecilestaller <c.staller96@gmail.com>
2023-12-03 18:37:27 +0100

    bilder importiert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 import './Home.scss'
+import allbeers from "./../../public/img/img_allbeers"
+import randombeer from "./../../public/img/img_randombeer"
 
 const Home = () => {
     return (  
         <section className="home_wrap">
             <article className="homeArticle_wrap">
                 <Link to='/allbeers'>
-                    <img src="./../public/img/img_allbeers.jpg" alt="viele flaschen bier" />
+                    <img src={allbeers} alt="viele flaschen bier" />
                     <h2>All Beers</h2>
                 </Link>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae asperiores quaerat ut, non dolorem eos.</p>
             </article>
             <article className="homeArticle_wrap">
                 <Link to='/randombeer'>
-                    <img src="./../public/img/img_randombeer.png" alt="viele gläser bier" />
+                    <img src={randombeer} alt="viele gläser bier" />
                     <h2>Random Beer</h2>
                 </Link>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae asperiores quaerat ut, non dolorem eos.</p>
